47f8538321f19e0c3ef52de6e5036401984a4541
SpOOk <spook.devv@gmail.com>
2023-07-05 14:40:20 -0400

    Adding StarRating component and default prop value

---
+++ src/StarRating.js
@@ -0,0 +1,32 @@
+// Styling objects
+const containerStyle = {
+  display: "flex",
+  alignItems: "center",
+  gap: "16px",
+};
+
+const starContainerStyle = {
+  display: "flex",
+  gap: "4px",
+};
+
+const textStyle = {
+  lineHeight: "1",
+  margin: "0",
+};
+
+// Star Rating component
+function StarRating({ maxRating = 5 }) {
+  return (
+    <div style={containerStyle}>
+      <div style={starContainerStyle}>
+        {Array.from({ length: maxRating }, (_, i) => (
+          <span>S{i + 1}</span>
+        ))}
+      </div>
+      <p style={textStyle}>10</p>
+    </div>
+  );
+}
+
+export default StarRating;
